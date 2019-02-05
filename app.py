54ff0706bc01e3d0cf62f6ba4ae3d0e24dff22a0
sarru1291 <sarru1291@gmail.com>
2019-02-05 12:45:12 +0530

    retinopathy added

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask,jsonify,request
+import json
+from PIL import Image
+import predict as pr
+import urllib
+import requests,shutil
+from StringIO import StringIO
 app = Flask(__name__)
 
+@app.route('/api/test',methods=['POST'])
+def imageTestPost():
+    incomingData=request.data
 
-@app.route('/api/test')
-def hello():
-    return "sarru"
+    incomingDataDic=json.loads(incomingData)
+    imageURL=incomingDataDic['url']
+    # print(incomingDataDic['url'])
+    # image=Image.open(urllib.urlopen(imageURL))
+    response=requests.get(imageURL,stream=True).raw
+    # print(image.format)
+    with open('my_image.jpg', 'wb') as file:
+        shutil.copyfileobj(response, file)
+    del response
+    # print(response)
+    output=pr.classifyDiabeticRetinopathy('my_image.jpg')
+    print(output)
+    # print(output['Severe case'])
+    # print(output['Mild case'])
+    # print(output['Retinopathy not detected'])
+    # joutput=json.dump(output)
+    # json.loads(joutput)
+    return jsonify({'output':output})
+
+@app.route('/api/test',methods=['GET'])
+def imageTestGet():
+    message='you have selected GET method'
+    return jsonify({'message':message})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ predict.py
@@ -0,0 +1,83 @@
+import tensorflow as tf, sys
+
+
+def classifySkinLesion(image_path):
+   
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+
+    label_lines = [line.rstrip() for line 
+                       in tf.gfile.GFile("HackathonProject/ISIC_3/retrained_labels.txt")]
+
+    with tf.gfile.FastGFile("HackathonProject/ISIC_3/retrained_graph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+    with tf.Session() as sess:
+    
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        
+        predictions = sess.run(softmax_tensor, \
+                 {'DecodeJpeg/contents:0': image_data})
+        
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            # print('%s (score = %.5f)' % (human_string, score))
+    # return('%s (score = %.5f)' % (human_string, score))
+        dic={
+            human_string:score
+        }
+    return dic
+
+
+def classifyDiabeticRetinopathy(image_path):
+   
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+    # Loads label file, strips off carriage return
+  #  label_lines = [line.rstrip() for line 
+   #                    in tf.gfile.GFile("HackathonProject/retino/retrained_labels.txt")]
+
+    # Unpersists graph from file
+    with tf.gfile.FastGFile("HackathonProject/retino/retrained_graph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+    with tf.Session() as sess:
+        # Feed the image_data as input to the graph and get first prediction
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        
+        predictions = sess.run(softmax_tensor, \
+                 {'DecodeJpeg/contents:0': image_data})
+        
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        
+        for node_id in top_k:
+            if (node_id==0):
+               #human_string = label_lines[node_id]
+               human_string1="Retinopathy not detected"
+               score1 = predictions[0][node_id]
+            #    print('%s (score = %.5f)' % (human_string1, score1))
+            elif (node_id==1):
+                human_string2="Mild case"
+                score2 = predictions[0][node_id]
+                # print('%s (score = %.5f)' % (human_string2, score2))
+            elif(node_id==2):
+                human_string3="Severe case"
+                score3 = predictions[0][node_id]
+                # print('%s (score = %.5f)' % (human_string3, score3))
+        dic={
+            human_string1:`score1`,
+            human_string2:`score2`,
+            human_string3:`score3`
+        }
+    # return('%s (score = %.5f)' % (human_string1, score1),(human_string2, score2),(human_string3, score3))
+    return dic
+   # return('%s (score = %.5f)' % (human_string2, score2))
+   # return('%s (score = %.5f)' % (human_string3, score3))
