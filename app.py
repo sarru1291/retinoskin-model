14aec54b4acb679cf2fc53a770f2eef8c41077df
sarru1291 <sarru1291@gmail.com>
2019-02-07 19:40:57 +0530

    exception handled

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,jsonify,request
 import json
 import predict as pr
-import requests,shutil
+import requests
 
 app = Flask(__name__)
 
@@ -11,21 +11,25 @@ def imageTestPost():
     incomingDataDic=json.loads(incomingData)
     imageURL=incomingDataDic['url']
     option=incomingDataDic['option']
-    response=requests.get(imageURL,stream=True).raw
-    with open('my_image.jpg', 'wb') as file:
-        shutil.copyfileobj(response, file)
-    del response
-    if option=='retinopathy':
-        output=pr.classifyDiabeticRetinopathy('my_image.jpg')
-    else:
-        output=pr.classifySkinLesion('my_image.jpg')
+    r=requests.get(imageURL,stream=True)
 
-    print(output)
+    if r.status_code!=200:
+        output='Model: Image fetching failed'
+    else:    
+        with open('my_image.jpg', 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+
+        if option=='retinopathy':
+            output=pr.classifyDiabeticRetinopathy('my_image.jpg')
+        else:
+            output=pr.classifySkinLesion('my_image.jpg')
+        
     return jsonify({'output':output})
 
 @app.route('/api/model/test',methods=['GET'])
 def imageTestGet():
-    message='you have selected GET method'
+    message='you have selected model GET method'
     return jsonify({'message':message})
 
 if __name__ == '__main__':
